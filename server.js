f4174acf43cddde6f7153dd2f0eca30be33ca7d7
Ryan Liang <ryan7x7x@gmail.com>
2019-07-22 20:52:04 -0700

    fixed port binding issue for Heroku hosting

--- server.js
+++ server.js
@@ -60,7 +60,5 @@ app.get('*', function (req, res) {
     res.send('Page not found 404').status(404);
   })
 
-app.listen(3000, () => {
-    console.log('Listening at http://localhost:3000');
-});
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
